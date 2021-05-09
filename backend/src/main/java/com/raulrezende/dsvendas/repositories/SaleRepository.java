b210a0302993c257646718a5c0e89064ff4c28d6
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-09 15:08:28 -0300

    Pagination

---
+++ backend/src/main/java/com/raulrezende/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.raulrezende.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raulrezende.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
